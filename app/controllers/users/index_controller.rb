8683bfcb0c753acf9e03cfc7a1209f3e2d7a7d5f
iL53n <ilsegribko@gmail.com>
2019-12-03 18:03:01 +0300

    done after check

--- app/controllers/users/index_controller.rb
+++ app/controllers/users/index_controller.rb
@@ -1,10 +1,10 @@
 class Users::IndexController < ApplicationController
-	before_action :authenticate_user!
+  before_action :authenticate_user!
 
 	def index; end
 
-	def user
-		@user = current_user
-		render json: @user
-	end
+  def user
+    @user = current_user
+    render json: @user
+  end
 end
