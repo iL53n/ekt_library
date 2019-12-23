36d36df5126d5da6746d0f3c0edf547d0191a9ab
iL53n <ilsegribko@gmail.com>
2019-12-24 00:31:23 +0300

    add left menu

--- app/javascript/components/Header.vue
+++ app/javascript/components/Header.vue
@@ -2,7 +2,7 @@
   div
     q-header(elevated class="bg-primary text-white" view="hHh lpR fFf")
       q-toolbar
-        q-btn(color="red" flat round dense icon="menu" @click="left = !left")
+        q-btn(flat round dense icon="menu" @click="left = !left")
         a(href="/")
           img(src="../images/Logo.png" alt="EKTlibrary" width="50" align="middle")
 
@@ -12,7 +12,7 @@
         div
           profile-menu
 
-      q-tabs(expand position="top" class="bg-primary text-white" dense inline-label align="center")
+      //q-tabs(expand position="top" class="bg-primary text-white" dense inline-label align="center")
         q-route-tab(to="/list_books" icon="library_books" label="Все книги")
         q-separator(dark vertical)
         q-route-tab(to="/reserved_books" icon="book" label="Зарезервировал")
@@ -23,22 +23,37 @@
         q-separator(dark vertical)
         q-route-tab(to="/wish_list" icon="favorite_border" label="Список желаний")
 
-    // q-drawer(show-if-above v-model="left" side="left" elevated)
-    q-drawer(v-model="left" side="left" elevated)
+    q-drawer(
+      v-model="left"
+      show-if-above
+
+      :mini="miniState"
+      @mouseover="miniState = false"
+      @mouseout="miniState = true"
+      mini-to-overlay
+
+      :width="220"
+      :breakpoint="500"
+      bordered
+      content-class="bg-grey-3")
       q-scroll-area(class="fit")
-        div(class="q-pa-sm")
-          q-tabs(vertical class="text-teal")
-            admin-left-menu
+        // добавить условие
+        admin-left-menu
+        q-separator
+        user-left-menu
+
 </template>
 
 <script>
   import ProfileMenu from './Profile'
-  import AdminLeftMenu from './AdminLeftMenu'
+  import AdminLeftMenu from './Tables/Admin/AdminLeftMenu'
+  import UserLeftMenu from './Tables/User/UserLeftMenu'
 
 	export default {
 		data: function () {
 			return {
-			  left: false
+			  left: false,
+        miniState: true
       }
 		},
 		created() {
@@ -47,7 +62,8 @@
 		},
 		components: {
       ProfileMenu,
-      AdminLeftMenu
+      AdminLeftMenu,
+      UserLeftMenu
 		}
 	}
 </script>
