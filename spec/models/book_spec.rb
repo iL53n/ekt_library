0670c2422e8372255d9318f627d7fd7b89806b14
iL53n <ilsegribko@gmail.com>
2019-12-24 13:09:42 +0300

    models tests

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -7,4 +7,7 @@ RSpec.describe Book, type: :model do
   it { should validate_presence_of :image }
   it { should validate_presence_of :status }
   it { should have_and_belong_to_many :categories }
+  it { should belong_to(:user).optional }
+  it { should have_many :readings }
+  it { should have_many :wishes }
 end
