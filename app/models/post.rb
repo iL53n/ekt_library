36b951c7a66f07e02cf33e45782b4655d452ba2a
iL53n <ilsegribko@gmail.com>
2020-01-22 22:40:38 +0300

    post model tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
   validates :title, inclusion: { in: %w[booking reading readed wish] }
+  validates :active, presence: true
 
   belongs_to :book
   belongs_to :user
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of :title }
+  it { should validate_inclusion_of(:title).in_array(%w[booking reading readed wish]) }
+  it { should validate_presence_of :active }
+  it { should belong_to :book }
+  it { should belong_to :user }
 end
