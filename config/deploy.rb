093c35cb8380871542fa6907be61a8541c58ae10
iL53n <ilsegribko@gmail.com>
2020-03-12 21:32:20 +0300

    add yarn to config deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,16 @@
+before "deploy:assets:precompile", "deploy:yarn_install"
+
+namespace :deploy do
+  desc 'Run rake yarn:install'
+  task :yarn_install do
+    on roles(:web) do
+      within release_path do
+        execute("cd #{release_path} && yarn install")
+      end
+    end
+  end
+end
+
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.12.1"
 # set :rvm_type, :user
