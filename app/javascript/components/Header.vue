0ef1837b57bba6d81f0a067355421f22e9f11f30
iL53n <ilsegribko@gmail.com>
2020-03-13 19:23:28 +0300

    add new logo

--- app/javascript/components/Header.vue
+++ app/javascript/components/Header.vue
@@ -3,10 +3,13 @@
     q-header(elevated class="bg-primary text-white" view="hHh lpR fFf" container)
       q-toolbar(class="toolbar")
         q-btn(flat round dense icon="menu" @click="left = !left")
-        a(href="/")
-          img(src="../images/Logo.png" alt="EKTlibrary" width="50" align="middle")
 
-        q-toolbar-title БИБЛИОТЕКА ЕКТ
+        q-item
+
+        a(href="/")
+          img(src="../images/logo_book_4.png" alt="EKTlibrary" width="60" align="middle")
+
+        q-toolbar-title БИБЛИОТЕКА
           q-badge(align="top" color="orange") v1.0.0b
 
         //div(class="toolbar-input-container justify-center")
