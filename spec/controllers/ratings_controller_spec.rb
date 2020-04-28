6644764c1ba0d7bcdaf069f1de2b723410805cff
iL53n <ilsegribko@gmail.com>
2020-04-28 12:33:24 +0300

    ratings controller spec - rubocop passed

--- spec/controllers/ratings_controller_spec.rb
+++ spec/controllers/ratings_controller_spec.rb
@@ -1,13 +1,30 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
+# rubocop:disable Metrics/BlockLength
 RSpec.describe RatingsController, type: :controller do
   let(:user) { create(:user) }
   before { login(user) }
 
   describe 'POST #create' do
     let(:book) { create(:book) }
-    let(:request_params) { { method: :post, action: :create, options: { rating: attributes_for(:rating), book_id: book, user_id: user }, format: :json } }
-    let(:request_invalid_params) { { method: :post, action: :create, options: { rating: attributes_for(:rating, :invalid), book_id: book, user_id: user }, format: :json } }
+    let(:request_params) do
+      { method: :post,
+        action: :create,
+        options: { rating: attributes_for(:rating),
+                   book_id: book,
+                   user_id: user },
+        format: :json }
+    end
+    let(:request_invalid_params) do
+      { method: :post,
+        action: :create,
+        options: { rating: attributes_for(:rating, :invalid),
+                   book_id: book,
+                   user_id: user },
+        format: :json }
+    end
 
     context 'with valid attributes' do
       it 'return :created' do
@@ -16,11 +33,13 @@ RSpec.describe RatingsController, type: :controller do
       end
 
       it 'save new rating in the database' do
-        expect { do_request(request_params) }.to change(Rating, :count).by(1)
+        expect { do_request(request_params) }
+          .to change(Rating, :count).by(1)
       end
 
       it 'does not save new rating(second) if user voted earlier' do
-        expect { 2.times { do_request(request_params) } }.to change(Rating, :count).by(1)
+        expect { 2.times { do_request(request_params) } }
+          .to change(Rating, :count).by(1)
       end
     end
 
@@ -31,8 +50,10 @@ RSpec.describe RatingsController, type: :controller do
       end
 
       it 'does not save new rating in the database' do
-        expect { do_request(request_invalid_params) }.to_not change(Rating, :count)
+        expect { do_request(request_invalid_params) }
+          .to_not change(Rating, :count)
       end
     end
   end
 end
+# rubocop:enable Metrics/BlockLength
