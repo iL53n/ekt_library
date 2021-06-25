00547a8168a7cf6239a9ef6b43848ad7488cf14e
iL53n <ilsegribko@gmail.com>
2021-06-25 13:00:28 +0300

    refactor(app): reserved table - in prigress

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,5 +1,5 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :start_date
+  attributes :id, :title, :start_date, :end_date
 
   belongs_to :book
   belongs_to :user
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
 	attributes :id, :email, :first_name, :last_name, :full_name_str, :admin, :created_at
 
-  has_many :books
+  # has_many :books
 
   def full_name_str
     "#{object.first_name} #{object.last_name}"
